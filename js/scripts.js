51ee4c876c98f8abbb68139d88bd4363e2d025b2
João Paulo Nascimento Gardiano <46694429+diczr@users.noreply.github.com>
2019-05-10 00:50:04 -0300

    First commit SideMenu W/ Bootstrap 4

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+                // Menu Mobile //
+
+
+                document.getElementById("menuIcon").onclick = function () {
+                    document.getElementById("slideOut-menu-action").style.display = "block";
+                };
+            
+                document.getElementById("closeIcon").onclick = function () {
+                    
+                    document.getElementById("slideOut-menu-action").style.display = "none";
+            
+                };
+
+
