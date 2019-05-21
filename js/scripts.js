d7d760fb05578e205c88283273e618b21bdc3617
João Paulo Nascimento Gardiano <46694429+diczr@users.noreply.github.com>
2019-05-21 12:15:33 -0300

    REFACTORING: SideMenu atualization

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 
-                // Menu Mobile //
-
-
-                document.getElementById("menuIcon").onclick = function () {
-                    document.getElementById("slideOut-menu-action").style.display = "block";
-                };
             
-                document.getElementById("closeIcon").onclick = function () {
-                    
-                    document.getElementById("slideOut-menu-action").style.display = "none";
+            function abrirMenu() {
+                document.getElementById("openSideMenu").style.width = "250px";
+                document.body.style.backgroundColor ="rgba(0,0,0,0.7)";
+                document.getElementById("main_menu").style.display = "none";
+            }
             
-                };
-
-
+            function fecharMenu() {
+                document.getElementById("openSideMenu").style.width = "0";
+                document.getElementById("openSideMenu").style.marginLeft = "0";
+                document.body.style.backgroundColor = "white";
+                document.getElementById("main_menu").style.display = "block";
+            }
